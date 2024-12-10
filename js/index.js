1ee0926636a22a72a0cac8cf92390a7e89ea1cee
nour24-3 <noorkeshk2005@gmail.com>
2024-12-10 19:15:43 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,58 @@
+
+let siteNameInput = document.getElementById("siteName");
+let urlLinkInput = document.getElementById("url");
+let bookmarkList = [];
+
+
+if(localStorage.getItem("bookmarkContainer") !== null){
+    bookmarkList = JSON.parse(localStorage.getItem("bookmarkContainer"));
+    displayData();
+    
+}
+
+
+
+function addBookmark(){
+let bookmark = {
+    Name : siteNameInput.value,
+    url : urlLinkInput.value,
+};
+    
+    bookmarkList.push(bookmark);
+    localStorage.setItem("bookmarkContainer" , JSON.stringify(bookmarkList));
+    displayData();
+    console.log(bookmarkList);
+    clearForm();
+
+}
+
+
+function clearForm(){
+    siteNameInput.value = null;
+    urlLinkInput.value = null;
+}
+
+
+function deleteItem(index){
+    bookmarkList.splice( index , 1  );
+    localStorage.setItem("bookmarkContainer" , JSON.stringify(bookmarkList));
+    displayData();
+}
+
+function displayData(){
+    let cartona =""
+    for(let i = 0 ; i < bookmarkList.length ; i++){
+
+        cartona +=`
+            <tr>
+                <th scope="row">${[i+1]}</th>
+                <td>${bookmarkList[i].Name}</td>
+                <td><button type="button" class="btn btn-success "><a href="${bookmarkList[i].url}"target="_blank"><span><i class="fa-solid fa-eye"></i></span>  View</a></button></td>
+                <td><button onclick="deleteItem(${i})" type="button" class="btn btn-danger"><span><i class="fa-solid fa-trash"></i></span> Delete</button></td>
+            </tr>
+                
+        `
+    }
+
+    document.getElementById("tData").innerHTML = cartona;
+}
